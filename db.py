6349dad03df86acd7349493461bac47e2234d5c3
Daniel Fairhead <daniel.fairhead@om.org>
2013-07-02 08:40:16 +0100

    more 'defaults' for testing.

--- db.py
+++ db.py
@@ -6,8 +6,12 @@ app.models.create_all()
 
 def make():
     ############################
-    # basic admin user.
-    if not app.models.User.select().where(app.models.User.loginname=='admin').exists():
+    # 3 basic users:
+    def user_exists(name):
+        return app.models.User.select() \
+                              .where(app.models.User.loginname==name).exists()
+
+    if not user_exists('admin'):
         user = app.models.User(loginname='admin', displayname='Admin',
                            emailaddress='admin@localhost', is_admin=True)
         user.set_password('password')
@@ -15,6 +19,21 @@ def make():
         user.save()
 
         group = app.models.Group(name='admins', display='Admins').save()
+
+    if not user_exists('jim'):
+        user = app.models.User(loginname='jim', displayname='James Hacker MP',
+                               emailaddress='jim@no10.gov.uk', is_admin=False)
+
+        user.set_password('password')
+        user.save()
+
+    if not user_exists('nobody'):
+        user = app.models.User(loginname='nobody', displayname='Nobody',
+                               emailaddress='devnull@localhost')
+
+        user.set_password('password')
+        user.save()
+
 
     ############################
     # default post types:
