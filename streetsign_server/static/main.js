6daecd0895f36a66f2abce39ca755801631fba92
Daniel Fairhead <daniel.fairhead@om.org>
2013-12-06 12:59:24 +0000

    removed some old crud, and cleaned up screen editor a lot.

--- streetsign_server/static/main.js
+++ streetsign_server/static/main.js
@@ -11,19 +11,6 @@ $('#flashed_notices > li').delay(15000).fadeOut('slow');
 
 $('select.chosen').chosen({'width':'100%'});
 
-////////////////////////////////////////////////
-// User login/details popup box thingy:
-
-$('#user_login_button').click( function () {
-    $('#login_box').fadeToggle(200, function (e){
-        $('input[name=username]').focus();
-        });
-});
-
-$('#login_box_close').click(function (e) {
-    $('#login_box').fadeOut();
-});
-
 /* Confirmation buttons: */
 
 $('.confirm_delete').click(function () {
