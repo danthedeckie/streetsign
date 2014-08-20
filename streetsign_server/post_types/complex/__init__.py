e254b5c9c02c98a73707e8c90951c49acf13e990
Daniel Fairhead <daniel.fairhead@om.org>
2014-08-20 16:13:25 +0100

    added complex html form

---
+++ streetsign_server/post_types/complex/__init__.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# StreetSign Digital Signage Project
+#     (C) Copyright 2013 Daniel Fairhead
+#
+#    StreetSign is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    StreetSign is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with StreetSign.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+---------------------------------
+streetsign_server.post_types.complex
+---------------------------------
+
+'ckeditor' Advanced HTML / rich text post type.
+
+"""
+
+__NAME__ = 'Advanced Document / HTML'
+__DESC__ = 'Complex HTML'
+
+from flask import render_template_string
+import re
+import bleach
+
+from streetsign_server.post_types import my
+
+def form(data):
+    ''' the form for editing this type of post '''
+    # pylint: disable=star-args
+    return render_template_string(my('form.html'), **data)
+
+def safehtml(text):
+    ''' used by 'receive' to clean html,
+        and not allow scripts and other nasties. '''
+
+    return bleach.clean(text, strip=True,
+        tags=["div", "span", "b", "i", "u",
+              "em", "ul", "li", "ol", "a", "br",
+              "code", "blockquote", "strong",
+              "small", "big", "img", "table",
+              "tr", "td", "th", "thead", "tbody",
+              "tfoot", "h1", "h2", "h3", "h4", "h5", "h6", "p"],
+        attributes=['class', 'href', 'alt', 'src', 'style', 'width','height','cellspacing', 'cellpadding', 'border' ],
+        styles=['background-color','color','height','width','font-family', 'text-align'])
+
+def safecolor(text, default="#fff"):
+    ''' check that a color string is actually a html hex-type color... '''
+    if not text:
+        return default
+    try:
+        return re.search("#[0-9a-fA-F]+", text).group()
+    except AttributeError:
+        return default
+
+def receive(data):
+    ''' turn the contents posted back to us from the form into
+        a dict which can be JSON'd by the system, and dumped as
+        text into the database. '''
+    #############
+    # TODO: sanify color input.
+
+    return {'type': 'complex',
+            'owntextcolor': data.get('owntextcolor', False),
+            'color': safecolor(data.get('color', False)),
+            'content': safehtml(data.get('content', ''))}
+
+def display(data):
+    ''' return the data ready for the display js to do stuff with. '''
+    return data['content']
+
+def screen_js():
+    ''' return the js needed to display this content. '''
+    return my('screen.js')
---
+++ streetsign_server/post_types/complex/screen.js
@@ -0,0 +1,32 @@
+{
+    render: function(zone, data) {
+        'use strict';
+        var newhtml = '';                   // html to append in to the zone.
+
+        console.log('making new complex html');
+
+        if (('type' in data.zone ) && ( data.zone.type == 'scroll')) {
+            newhtml = $('<div class="post post_html post_scrolling"><div class="post_inner">'
+                           + magic_vars(data.content.content).replace('<br/>',' ')
+                           + '</div></div>').prependTo(zone);
+        } else {
+
+            newhtml = $('<div class="post post_html"><div class="post_inner">'
+                    + magic_vars(data.content.content)
+                    + '</div></div>')
+                    .prependTo(zone);
+        }
+
+        // Reduce font size until it fits nicely:
+
+        reduce_font_size_to_fit(newhtml.children('.post_inner'), $(zone));
+
+        // Set font color:
+        if (data.content.owntextcolor) {
+            try{newhtml.css('color',data.content.color);}catch(e){};
+        }
+        // Return the HTML object, hiding it along the way.
+
+        return newhtml;
+    }
+}
