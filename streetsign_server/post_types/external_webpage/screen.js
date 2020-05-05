cd586d73e92294e686f1327ecd74ade12c6a984e
Daniel Lang <ldaniell14260@gmail.com>
2020-05-05 21:47:32 +0200

    font size fix for webkit browsers
    For some unknown reason external webpages shown in an iframe are moved up because of the dynamically calculated font size.
    This seems to only affect iframes and only webkit based browsers (safari, epiphany,...).
    The fix for this to add an extra class to the container for the iframe which resets the font size to zero.
    Testing showed that is does not have an effect on other engines or post types.

--- streetsign_server/post_types/external_webpage/screen.js
+++ streetsign_server/post_types/external_webpage/screen.js
@@ -10,11 +10,11 @@
                   + '">...</iframe>');
 
         if (('type' in data.zone ) && ( data.zone.type == 'scroll')) {
-            newhtml = $('<div class="post post_html post_scrolling"><div class="post_inner">'
+            newhtml = $('<div class="post post_html post_scrolling"><div class="post_inner post_reset_fontsize">'
                         + newhtml
                         + '</div></div>').prependTo(zone);
         } else {
-            newhtml = $('<div class="post post_html"><div class="post_inner">'
+            newhtml = $('<div class="post post_html"><div class="post_inner post_reset_fontsize">'
                     + newhtml
                     + '</div></div>')
                     .prependTo(zone);
